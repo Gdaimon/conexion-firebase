a2fe2a98bf50faa7c455f3777af4bc62771da710
Carlos Andres Charris S <darkklitos@gmail.com>
2021-10-12 20:12:12 -0500

    Commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
 import './App.css';
+import { actualizarDocumentoDatabase, consultarDatabase, consultarDocumentoDatabase, crearUsuario, datosUsuario, eliminarDocumentoDatabase, guardarDatabase, loginUsuario, logOutUsuario, usuario } from './config/firebase';
 
 function App() {
 
-  const handleClick = () => {
+  const handleClick = async () => {
     console.log('Entro');
+
+    // const usuario = {
+    //   nombre: 'Pedro',
+    //   edad: 30
+    // }
+
+    // CRUD => CREATE, READ, UPDATE, DELETE
+    //  CREAR, LEER, ACTUALIZAR, BORRAR
+
+    // Guardar en base de datos
+    // guardarDatabase('usuarios', usuario);
+
+    // Obtener todos los documentos de la colleccion
+    // consultarDatabase('usuarios')
+    // console.log(await consultarDatabase('usuarios'));
+
+    // Obtener un documento id=CEqaCqjBFnI0SQKRj0tI
+    // consultarDocumentoDatabase('usuarios', 'CEqaCqjBFnI0SQKRj0tI')
+
+    const usuarioDos = {
+      nombre: 'Martha',
+      edad: 15
+    }
+
+    // Actualizacion documento  id=CEqaCqjBFnI0SQKRj0tI
+    // actualizarDocumentoDatabase('usuarios', 'CEqaCqjBFnI0SQKRj0tI', usuarioDos)
+
+    // Eliminar documento  id=CEqaCqjBFnI0SQKRj0tI
+    // eliminarDocumentoDatabase('usuarios', 'CEqaCqjBFnI0SQKRj0tI', usuarioDos)
+
+    // Creacion Usuario
+    // crearUsuario('darkklitos@gmail.com', '123456')
+
+    // Login
+    // await loginUsuario('darkklitos@gmail.com', '123456')
+    console.log('datos usuario: ', usuario);
+
+    //  salir -> LogOut
+    // logOutUsuario()
+
+    //  Datos Usuario
+    // datosUsuario()
+
+
   }
 
   return (
